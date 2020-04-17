4a2400c2be9a28d517da37c9ccc0970eb490edcf
Lim Kyung Min <limup3>
2020-04-17 18:08:07 +0900

    first commit

---
+++ java/com/occamsrazor/web/Homecontroller.java
@@ -0,0 +1,19 @@
+package com.occamsrazor.web;
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootConfiguration
+@Controller
+
+public class Homecontroller {
+
+	@GetMapping("/")
+	public String hello() {
+		return "index.html";
+	}
+
+	
+}
